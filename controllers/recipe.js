1144e6de3fa2860abc59d1403c96ac127aa71626
Hamza Lusigi <46598085+vonmicky@users.noreply.github.com>
2019-03-31 15:12:32 +0300

    Added recipe controller

---
+++ controllers/recipe.js
@@ -0,0 +1,93 @@
+const Recipe = require('../models/recipe');
+
+exports.getRecipes = (req, res, next) => {
+  Recipe.find().then(
+    (recipes) => {
+      res.status(200).json(recipes);
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.getSingleRecipe = (req, res, next) => {
+  Recipe.findOne({ _id: req.params.id}).then(
+    (recipe) => {
+      res.status(200).json(recipe);
+    }
+  ).catch(
+    (error) => {
+      res.status(404).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.saveRecipe = (req, res, next) => {
+ 	const recipe = new Recipe({
+ 	  title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+  recipe.save().then(
+    () => {
+      res.status(201).json({
+        message: 'Recipe saved successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+}; 
+
+exports.modifyRecipe = (req, res, next) => {
+	const recipe = new Recipe({
+    _id: req.params.id,
+    title: req.body.title,
+    ingredients: req.body.ingredients,
+    instructions: req.body.instructions,
+    difficulty: req.body.difficulty,
+    time: req.body.time
+  });
+  Recipe.updateOne({_id: req.params.id}, recipe).then(
+    () => {
+      res.status(201).json({
+        message: 'Recipe updated successfully!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );
+};
+
+exports.deleteRecipe = (req, res, next) => {
+  Recipe.deleteOne({_id: req.params.id}).then(
+    () => {
+      res.status(200).json({
+        message: 'Deleted!'
+      });
+    }
+  ).catch(
+    (error) => {
+      res.status(400).json({
+        error: error
+      });
+    }
+  );	
+};
+
